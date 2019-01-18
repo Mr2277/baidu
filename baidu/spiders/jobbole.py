99149212a789aa5a6e355062ef942dca6306ecd6
sun <837544713@qq.com>
2019-01-18 11:27:53 +0800

    add 基础选择器

--- baidu/spiders/jobbole.py
+++ baidu/spiders/jobbole.py
@@ -1,13 +1,22 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy import Request
+from scrapy.commands import parse
 
 
 class JobboleSpider(scrapy.Spider):
     name = 'jobbole'
     allowed_domains = ['blog.jobbole.com']
-    start_urls = ['http://blog.jobbole.com/']
+    start_urls = ['http://blog.jobbole.com/all-posts/']
 
     def parse(self, response):
-        print("tttt")
-        print(response)
+        #print("tttt")
+        #print(response)
+        #print(response.css)
+        post_nodes = response.css("#archive .floated-thumb .post-thumb")  # a selector, 可以在这个基础上继续做 selector
+        print(post_nodes)
+        for post_node in post_nodes:
+            post_url = post_node.css("a::attr(href)").extract_first("")
+            yield Request(url=parse.urljoin(response.url, post_url),callback=self.parse_detail)
+
         pass
